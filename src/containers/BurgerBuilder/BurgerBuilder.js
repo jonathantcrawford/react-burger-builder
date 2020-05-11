e03e9427d0329ec721a08bd055ea082073a51483
jonathantcrawford <jon_t_crawford@protonmail.com>
2020-05-10 21:59:29 -0400

    completed udemy 227

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,36 +94,11 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // alert('You continue');
-        //this.setState({loading: true});
-        //const order = {
-        //    ingredients: this.state.ingredients,
-        //    price: this.state.totalPrice,
-        //    customer: {
-        //        name: 'Jon',
-        //        address: {
-        //            street: 'test 1',
-        //            zipCode: '12345',
-        //            country: 'USA'
-        //        },
-        //        email: 'test@test.com'
-        //    },
-        //    deliveryMethod: 'fastest'
-        //};
-        //
-        //axios.post('/orders.json', order)
-        //    .then(response => {
-        //        console.log(response);
-        //        this.setState({loading: false, purchasing: false});
-        //    })
-        //    .catch(error => {
-        //        console.log(error);
-        //        this.setState({loading: false, purchasing: false});
-        //    });
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
 
         this.props.history.push({
