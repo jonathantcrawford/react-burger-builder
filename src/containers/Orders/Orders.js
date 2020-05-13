32b22327d72c76efac9bb6690a2a30277c068605
jonathantcrawford <jon_t_crawford@protonmail.com>
2020-05-13 10:29:13 -0400

    completed udemy 332

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -14,7 +14,9 @@ class Orders extends Component {
     }
 
     componentDidMount() {
-        this.props.onFetchOrders();
+        if (this.props.token) {
+            this.props.onFetchOrders(this.props.token);
+        }
     }
 
     render () {
@@ -39,13 +41,14 @@ class Orders extends Component {
 const mapStateToProps = state => {
     return {
         orders: state.order.orders,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders: () => dispatch(actions.fetchOrders())
+        onFetchOrders: (token) => dispatch(actions.fetchOrders(token))
     }
 }
 
