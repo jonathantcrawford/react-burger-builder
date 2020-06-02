6a490686ffde9cece56bbee805a6a3cf77f46a76
jonathantcrawford <jon_t_crawford@protonmail.com>
2020-06-02 18:12:14 -0400

    cnvrt modal, create custom hook

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,7 +5,6 @@ import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 const modal = props => {
-
     return (
         <Aux>
             <Backdrop show={props.show} clicked={props.modalClosed} />
